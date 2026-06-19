bf786e7d1f8cfad3c8b2e493c9dcaccc55b71fd8
Saif <saiffkhann.5@gmail.com>
2026-06-19 16:40:29 +0300

    Phase 1: print_board function with nested loops

--- board.py
+++ board.py
@@ -14,5 +14,24 @@ board = [
     [1, 0, 1, 0, 1, 0, 1, 0],
     [0, 1, 0, 1, 0, 1, 0, 1],
     [1, 0, 1, 0, 1, 0, 1, 0],
-    
 ]
+
+def print_board(board):
+    print("  0  1  2  3  4  5  6  7")
+    for row_index in range(8):
+        print(row_index, end=" ")
+        for col_index in range(8):
+            piece = board[row_index][col_index]
+            if piece == 0:
+                print(" .", end=" ")
+            elif piece == 1:
+                print(" w", end=" ")
+            elif piece == -1:
+                print(" b", end=" ")
+            elif piece == 2:
+                print(" W", end=" ")
+            elif piece == -2:
+                print(" B", end=" ")
+        print()
+
+print_board(board)
